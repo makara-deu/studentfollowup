b210be9076a5fefd2d109ced583b832a2d997640
makara deu <makara.deu99@gmail.com>
2020-07-03 13:51:55 +0700

    migration file

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,14 +14,43 @@ class CreateUsersTable extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
+            $table->bigIncrements('id');
+            $table->string('firstName');
+            $table->string('lastName');
+            $table->integer('role')->default(0); //0:normal; 1:admin
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->unsignedBigInteger('position_id');
+            $table->foreign('position_id')
+                  ->references('id')
+                  ->on('positions')
+                  ->onDelete('cascade');
             $table->rememberToken();
             $table->timestamps();
         });
+         // insert default admin user
+         DB::table('users')->insert(
+            array(
+                'firstName' => "admin",
+                'lastName' => "user",
+                'email' => "admin@example.com",
+                'position_id' => 1,
+                'role' =>1,
+                'password' => bcrypt('password'),
+                'remember_token' => Str::random(10),
+            )
+        );
+        DB::table('users')->insert(
+            array(
+                'firstName' => "normal",
+                'lastName' => "user",
+                'email' => "normal@example.com",
+                'position_id' => 4,
+                'password' => bcrypt('password'),
+                'remember_token' => Str::random(10),
+            )
+        );
     }
 
     /**
